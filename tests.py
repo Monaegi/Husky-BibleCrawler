1c4f0410229bb32cbcfd69a5b89f1472dea666f5
huskyhoochu <dfg1499@gmail.com>
2018-01-02 16:30:19 +0900

    UnitTest 추가

--- tests.py
+++ tests.py
@@ -1,5 +1,24 @@
-import pytest
+import unittest
+
+import crawler
 
 
-def test_create_soup():
-    pass
+class CrawlerTest(unittest.TestCase):
+    def setUp(self):
+        self.base_url = crawler.BASE_URL
+        self.params = crawler.PAYLOAD
+
+    def test_requests_from_catholic_goodnews(self):
+        r = crawler.requests_from_catholic_goodnews(
+            url=self.base_url,
+            payload=self.params,
+        )
+        self.assertEqual(r.status_code, 200)
+
+    def test_soup(self):
+        soup = crawler.soup_from_requests()
+        self.assertEqual(soup.tag == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
