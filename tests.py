3b30fc2114d7088e7303c0385d5bf3474b460446
huskyhoochu <dfg1499@gmail.com>
2018-01-02 17:40:24 +0900

    test에 주석 추가

--- tests.py
+++ tests.py
@@ -5,6 +5,14 @@ import crawler
 
 class CrawlerTest(unittest.TestCase):
     def setUp(self):
+        """
+        크롤러 테스트를 위한 전역변수 설정
+        1. base_url: 크롤링을 하기 위한 가톨릭 굿뉴스 성경 사이트
+        2. params: 신약성경, 구약성경 및 각 복음서의 고유 pk 값, 페이지
+        3. requests: Requests 객체
+        4. soup: BeautifulSoup 객체
+        :return: None
+        """
         self.base_url = crawler.BASE_URL
         self.params = crawler.PAYLOAD
         self.requests = crawler.requests_from_catholic_goodnews(
@@ -14,14 +22,26 @@ class CrawlerTest(unittest.TestCase):
         self.soup = crawler.soup_from_requests(self.requests)
 
     def test_requests_from_catholic_goodnews(self):
+        """
+        Request 객체가 정상적으로 생성되어 200 응답코드를 주는지 테스트
+        :return: None
+        """
         requests = self.requests
         self.assertEqual(requests.status_code, 200)
 
     def test_soup_is_exist(self):
+        """
+        BeautifulSoup 객체가 정상적으로 생성되는지 테스트
+        :return: None
+        """
         soup = self.soup
         self.assertFalse(soup.can_be_empty_element)
 
     def test_select_texts_from_soup(self):
+        """
+        BS 객체에서 뽑아낸 성경 구절 리스트가 정상적으로 생성되는지 테스트
+        :return:
+        """
         li = crawler.texts_from_soup(self.soup)
         self.assertEqual(type(li), type(list()))
 
